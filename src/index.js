103628167265680e9ba6282901a796a764a8b355
MestreJuan <juangregoriodecastro@gmail.com>
2026-02-06 21:41:22 -0300

    começando segurança

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //Como uma funcao main() em C, carregando bibliotecas e fazendo o loop de servidor
 require('dotenv').config(); //funcao que carrega os dados do .env
 const express = require('express'); //localiza a pasta express dentro do node_modules e retorna a funcao que gera o framework
+const helmet = require('helmet'); //importa a segurança helmet para o servidor
 const app = express(); //cria uma instancia de servidor com as configuracoes do site usando a funcao que foi puxada pelo express
+app.use(helmet()); //sa a u
 const rotasProdutos = require('./routes/produtos'); //importa o arquivo de rotas de produtos, executa ele e guarda o endereço na variavel
 const rotasUsuarios = require('./routes/usuarios'); //importa o arquivo de rotas de usuarios, executa ele e guarda o endereço na variavel
 const rotasPedidos = require('./routes/pedidos'); //importa o arquivo de rotas de pedidos, executa ele e guarda o endereço na variavel
@@ -16,4 +18,4 @@ app.use('/pedidos', rotasPedidos); //manda para o arquivo de rota pedidos se a U
 
 const PORT = process.env.PORT || 3000; //faz tentativa de mandar para a porta do arquivo .env, caso nao exista, manda pro 3000
 
-app.listen(PORT, () => { console.log(`🚀 Servidor rodando em: http://localhost:${PORT}`); }); //inicia o loop do servidor como se fosse um while(1) e apenas quando conseguir ocupar a porta de destino, imprime o .log
+app.listen(PORT, '0.0.0.0', () => { console.log(`🚀 Servidor aberto na rede com a porta:${PORT}`); }); //inicia o loop do servidor como se fosse um while(1) e apenas quando conseguir ocupar a porta de destino, imprime o .log
